d7ff506ba4afb69f42df2e7e4436f2c8c267de5a
Gavmo <1gavmo@gmail.com>
2024-01-01 12:28:26 +1100

    Split the string for iteration.

--- app/js/script.js
+++ app/js/script.js
@@ -12,16 +12,6 @@ function findHighIndexCharacters(inputtext) {
    return charindex
 }
 
-function highlightBadChars(inputtext) {
-    var precedingstring = document.createElement("span");
-    precedingstring.innerHTML = inputtext.substring(0, inputtext.length - 1);
-    var newspan = document.createElement("span");
-    newspan.setAttribute("class", "bg-danger");
-    newspan.innerHTML = inputtext.slice(-1);
-    return [precedingstring, newspan]
-
-}
-
 function replaceBrackets(inputtext) {
     return inputtext.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 }
@@ -30,25 +20,16 @@ function replaceNewline(inputtext) {
     return inputtext.replaceAll("\n", "<br>");
 }
 
-function makeNewhighightedString(inputtext, indexes) {
-    var lastindex = 0;
-    var return_array = [];
-    indexes.forEach(i => {
-        // offset by 1 so the last character is the bad char.
-        char_substring = inputtext.substring(lastindex, i + 1);
-        lastindex = i + 1;
-        highlightBadChars(char_substring).forEach(x => {
-            return_array.push(x);
-        })
-    
-    });
-    // Add the last part of the input string to the array
-    var last_element = document.createElement("span");
-    last_element.innerHTML = inputtext.substring(lastindex, inputtext.length);
-    return_array.push(last_element);
-    return return_array;
-}
+function makeNewhighightedString(inputtext) {
+    var indexes = inputtext.split('');
+    for (var i = 0; i < indexes.length; i++) {
+        if (indexes[i].charCodeAt(0) > 127) {
+            indexes[i] = `<span class=\"bg-danger\ rounded">${indexes[i]}</span>`
+        }
 
+    }
+    return indexes.join("");
+}
 
 function updateCharcount() {
     var inputlength = document.getElementById("id_inputtext").value.length;
@@ -61,14 +42,8 @@ function updateOutput() {
     var outputtext = replaceBrackets(document.getElementById("id_inputtext").value);
     outputtext = replaceNewline(outputtext);
     document.getElementById("id_output").innerHTML = "";
-    var outputarray = makeNewhighightedString(outputtext, findHighIndexCharacters(outputtext));
-    
-    outputarray.forEach( el => {
-        document.getElementById("id_output").appendChild(el);    
-    }
-
-    )
-    //document.getElementById("id_output").innerHTML = outputtext;
+    outputtext = makeNewhighightedString(outputtext);
+    document.getElementById("id_output").innerHTML = outputtext;
 }
 
 
